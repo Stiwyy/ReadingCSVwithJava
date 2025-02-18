06b0f47e6e9b6532758274aaa7a2d683eb2d97fa
Stiwyy <andrin.renggli@lernende.bbw.ch>
2025-02-18 10:39:29 +0100

    initial commit

--- src/main/java/org/example/Championship.java
+++ src/main/java/org/example/Championship.java
@@ -1,4 +1,147 @@
 package org.example;
 
+import java.util.Date;
+
 public class Championship {
+    int blub_idfs;
+    int id;
+    int year;
+    Date date_from;
+    Date date_to;
+    String championship;
+    String city;
+    String country;
+    Date event_date;
+    String event_time_from;
+    String event_time_to;
+    String event_title;
+
+    public Championship(int blub_idfs, int id, int year, Date date_from, Date date_to, String championship, String city, String country, Date event_date, String event_time_from, String event_time_to, String event_title) {
+        this.blub_idfs = blub_idfs;
+        this.id = id;
+        this.year = year;
+        this.date_from = date_from;
+        this.date_to = date_to;
+        this.championship = championship;
+        this.city = city;
+        this.country = country;
+        this.event_date = event_date;
+        this.event_time_from = event_time_from;
+        this.event_time_to = event_time_to;
+        this.event_title = event_title;
+    }
+
+    public int getBlub_idfs() {
+        return blub_idfs;
+    }
+
+    public void setBlub_idfs(int blub_idfs) {
+        this.blub_idfs = blub_idfs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Date getDate_from() {
+        return date_from;
+    }
+
+    public void setDate_from(Date date_from) {
+        this.date_from = date_from;
+    }
+
+    public Date getDate_to() {
+        return date_to;
+    }
+
+    public void setDate_to(Date date_to) {
+        this.date_to = date_to;
+    }
+
+    public String getChampionship() {
+        return championship;
+    }
+
+    public void setChampionship(String championship) {
+        this.championship = championship;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getEvent_date() {
+        return event_date;
+    }
+
+    public void setEvent_date(Date event_date) {
+        this.event_date = event_date;
+    }
+
+    public String getEvent_time_from() {
+        return event_time_from;
+    }
+
+    public void setEvent_time_from(String event_time_from) {
+        this.event_time_from = event_time_from;
+    }
+
+    public String getEvent_time_to() {
+        return event_time_to;
+    }
+
+    public void setEvent_time_to(String event_time_to) {
+        this.event_time_to = event_time_to;
+    }
+
+    public String getEvent_title() {
+        return event_title;
+    }
+
+    public void setEvent_title(String event_title) {
+        this.event_title = event_title;
+    }
+
+    @Override
+    public String toString() {
+        return "Championship{" +
+                "blub_idfs=" + blub_idfs +
+                ", id=" + id +
+                ", year=" + year +
+                ", date_from=" + date_from +
+                ", date_to=" + date_to +
+                ", championship='" + championship + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", event_date=" + event_date +
+                ", event_time_from='" + event_time_from + '\'' +
+                ", event_time_to='" + event_time_to + '\'' +
+                ", event_title='" + event_title + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,141 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String filePath = "src/main/resources/realtime.csv";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(";");
+
+                String datum = tokens[8];
+                String ort = tokens[6];
+                String titel = tokens[11];
+
+                if (istImZeitraum(datum)) {
+                    System.out.println("Ort: " + ort + " | Titel: " + titel);
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        /*
+        ================================================================================
+         */
+
+        List<Championship> championships = new ArrayList<>();
+
+        championships.add(new Championship(
+                515, 3253, 2013,
+                new Date(113, 10, 19), new Date(113, 10, 25),
+                "ISSF World Cup Munich", "Munich", "GER",
+                new Date(113, 10, 20), "14:30:00", "15:00:00", "Final 10m Air Rifle Men"));
+
+        championships.add(new Championship(
+                516, 3254, 2013,
+                new Date(113, 10, 19), new Date(113, 10, 25),
+                "ISSF World Cup Munich", "Munich", "GER",
+                new Date(113, 10, 21), "17:00:00", "17:30:00", "Final Trap Women"));
+
+        championships.add(new Championship(
+                517, 3255, 2013,
+                new Date(113, 10, 19), new Date(113, 10, 25),
+                "ISSF World Cup Munich", "Munich", "GER",
+                new Date(113, 10, 22), "15:05:00", "15:35:00", "Final 10m Air Rifle Women"));
+
+        championships.add(new Championship(
+                518, 3256, 2013,
+                new Date(113, 10, 19), new Date(113, 10, 25),
+                "ISSF World Cup Munich", "Munich", "GER",
+                new Date(113, 10, 23), "13:00:00", "13:30:00", "Final 50m Pistol Men"));
+
+        championships.add(new Championship(
+                519, 3257, 2013,
+                new Date(113, 10, 19), new Date(113, 10, 25),
+                "ISSF World Cup Munich", "Munich", "GER",
+                new Date(113, 10, 24), "16:30:00", "17:00:00", "Final Trap Men"));
+
+        for (Championship c : championships) {
+            System.out.println(championships.toString());
+        }
+
+        /*
+        ==========================================================================================
+         */
+
+        List<Championship> championships1 = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(";");
+
+                int blub_idfs = Integer.parseInt(tokens[0]);
+                int id = Integer.parseInt(tokens[1]);
+                int year = Integer.parseInt(tokens[2]);
+                Date date_from = dateFormat.parse(tokens[3]);
+                Date date_to = dateFormat.parse(tokens[4]);
+                String championship = tokens[5];
+                String city = tokens[6];
+                String country = tokens[7];
+                Date event_date = dateFormat.parse(tokens[8]);
+                String event_time_from = tokens[9];
+                String event_time_to = tokens[10];
+                String event_title = tokens[11];
+
+                Championship championshipObj = new Championship(
+                        blub_idfs, id, year, date_from, date_to,
+                        championship, city, country, event_date, event_time_from, event_time_to, event_title
+                );
+                championships1.add(championshipObj);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Championship c : championships) {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("Championship: " + c.getChampionship());
+            System.out.println("Ort: " + c.getCity());
+            System.out.println("Land: " + c.getCountry());
+            System.out.println("Ereignisdatum: " + c.getEvent_date());
+            System.out.println("Ereigniszeit von: " + c.getEvent_time_from());
+            System.out.println("Ereigniszeit bis: " + c.getEvent_time_to());
+            System.out.println("Titel: " + c.getEvent_title());
+
+        }
+
+
+
+    }
+
+    private static boolean istImZeitraum(String datum) {
+        try {
+            String[] parts = datum.split("\\.");
+            int tag = Integer.parseInt(parts[0]);
+            int monat = Integer.parseInt(parts[1]);
+            int jahr = Integer.parseInt(parts[2]);
+
+            return (jahr == 2005 && monat == 11 && tag >= 19 && tag <= 25);
+        } catch (Exception e) {
+            return false;
         }
     }
+
+
 }
